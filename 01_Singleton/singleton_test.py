83a3c448e2c3ca6bdc2117228eae0d6dc9eb931b
MPilkou <mpilkou@gmail.com>
2019-03-27 12:00:08 +0100

    singleton changes

--- 01_Singleton/singleton_test.py
+++ 01_Singleton/singleton_test.py
@@ -1,12 +1,16 @@
 import unittest
 import pickle as pic
+import os
 
 from multiprocessing.pool import ThreadPool as Pool
-from singleton import *
+from singleton import Singleton, ChildSingleton
 
 # Python 2/3 compatibility
 if not hasattr(unittest.TestCase, 'assertCountEqual'):
-    unittest.TestCase.assertCountEqual = unittest.TestCase.assertItemsEqual
+    try:
+        unittest.TestCase.assertCountEqual = unittest.TestCase.assertItemsEqual
+    except:
+        print("Warning")
 
 """Help fun"""
 def help_threat_get_singleton():
@@ -59,7 +63,12 @@ class SingletonTest(unittest.TestCase):
             pool2 = Pool(processes=1)
             self.assertEqual(pool1.apply_async(help_threat_read_singleton()).get(), pool2.apply_async(help_threat_read_singleton()).get())
         except:
-            print ("Error")
+            print ("Error in test")
+
+        try:
+            os.remove("singleton")
+        except OSError:
+            print ("Error in remove")
 
 if __name__ == '__main__':
     unittest.main()
