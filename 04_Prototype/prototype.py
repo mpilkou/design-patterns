9cc8c10a1e0dda26560e9cda6d33d4a9427b208a
MPilkou <mpilkou@gmail.com>
2019-04-06 11:59:40 +0200

    add prototype

---
+++ 04_Prototype/prototype.py
@@ -0,0 +1,73 @@
+from abc import ABC, abstractmethod
+import copy
+
+class Prototype(ABC):
+
+    def __init__(self):
+        self.__name = None
+        self.__value = None
+
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @property
+    def value(self):
+        return self.__value
+    @value.setter
+    def value(self, value):
+        self.__value = value
+
+    @abstractmethod
+    def clone(self):
+        return copy.copy(self)
+
+class Type_1_Prototype(Prototype):
+    def __init__(self, value):
+      self.name = "Type_1"
+      self.value = value
+
+    def __call__(self):
+        return self.clone()
+
+    def clone(self):
+        return super().clone()
+
+    
+class Type_2_Prototype(Prototype):
+    def __init__(self, value):
+      self.name = "Type_2"
+      self.value = value
+    
+    def __call__(self):
+        return self.clone()
+
+    def clone(self):
+        return super().clone()
+
+class Type_2_1_Prototype(Type_2_Prototype):
+    def __init__(self, value):
+      self.name = "Type_2_1"
+      self.value = value
+
+    def __call__(self):
+        return self.clone()
+
+    def clone(self):
+        return super().clone()
+
+class Type_2_2_Prototype(Type_2_Prototype):
+    def __init__(self, value):
+      self.name = "Type_2_2"
+      self.value = value
+
+    def __call__(self):
+        return self.clone()
+        
+    def clone(self):
+        return super().clone()
+        
+
---
+++ 04_Prototype/prototype_test.py
@@ -0,0 +1,32 @@
+import unittest
+import pickle as pic
+import warnings
+
+from multiprocessing.pool import ThreadPool as Pool
+
+from prototype import Prototype, Type_1_Prototype, Type_2_Prototype, Type_2_1_Prototype, Type_2_2_Prototype
+
+# Python 2/3 compatibility
+if not hasattr(unittest.TestCase, 'assertCountEqual'):
+    try:
+        unittest.TestCase.assertCountEqual = unittest.TestCase.assertItemsEqual
+    except:
+        print("Warning")
+
+class PrototypeTest(unittest.TestCase):
+    """Prototype"""
+
+    def test_prototype_clone(self):
+        type1 = Type_1_Prototype(123)
+        self.assertEqual(type(Type_1_Prototype(321)), type(type1.clone()))
+
+    def test_prototype_by_type_1(self):
+        type1   = Type_1_Prototype(111)
+        self.assertNotEqual(type1(), type1())
+
+    def test_prototype_by_type_2(self):
+        type2_1     = Type_2_1_Prototype(111)
+        self.assertNotEqual(type2_1(), type2_1())
+
+if __name__ == '__main__':
+    unittest.main()
