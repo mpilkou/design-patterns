3d0f5b62f2c92572061e6d5c95bde1c37f64f9bb
matt <mpilkou@gmail.com>
2020-03-09 16:21:01 +0300

    use pep8

--- 02_AbstractFactory/factory_test.py
+++ 02_AbstractFactory/factory_test.py
@@ -4,9 +4,9 @@ import warnings
 
 from multiprocessing.pool import ThreadPool as Pool
 
-from factories.large_factory    import LargeFactory
-from factories.medium_factory   import MediumFactory
-from factories.small_factory    import SmallFactory
+from factories.large_factory import LargeFactory
+from factories.medium_factory import MediumFactory
+from factories.small_factory import SmallFactory
 
 # Python 2/3 compatibility
 if not hasattr(unittest.TestCase, 'assertCountEqual'):
@@ -15,29 +15,54 @@ if not hasattr(unittest.TestCase, 'assertCountEqual'):
     except:
         print("Warning")
 
+
 class FactoryTest(unittest.TestCase):
     """Factory"""
 
     def test_large_factory(self):
         factory = LargeFactory()
-        self.assertEqual(factory.createButton().getHtml(),   "<button size = 5 >  </button>")
-        self.assertEqual(factory.createHeader().getHtml(),   "<h size = 5 >  </h>")
-        self.assertEqual(factory.createImage().getHtml(),    "<image size = 5 >  </image>")
-        self.assertEqual(factory.createText().getHtml(),     "<p size = 5 >  </p>")
+        self.assertEqual(
+            factory.createButton().getHtml(),
+            "<button size = 5 >  </button>")
+        self.assertEqual(
+            factory.createHeader().getHtml(),
+            "<h size = 5 >  </h>")
+        self.assertEqual(
+            factory.createImage().getHtml(),
+            "<image size = 5 >  </image>")
+        self.assertEqual(
+            factory.createText().getHtml(),
+            "<p size = 5 >  </p>")
 
     def test_medium_factory(self):
         factory = MediumFactory()
-        self.assertEqual(factory.createButton().getHtml(),   "<button size = 3 >  </button>")
-        self.assertEqual(factory.createHeader().getHtml(),   "<h size = 3 >  </h>")
-        self.assertEqual(factory.createImage().getHtml(),    "<image size = 3 >  </image>")
-        self.assertEqual(factory.createText().getHtml(),     "<p size = 3 >  </p>")
+        self.assertEqual(
+            factory.createButton().getHtml(),
+            "<button size = 3 >  </button>")
+        self.assertEqual(
+            factory.createHeader().getHtml(),
+            "<h size = 3 >  </h>")
+        self.assertEqual(
+            factory.createImage().getHtml(),
+            "<image size = 3 >  </image>")
+        self.assertEqual(
+            factory.createText().getHtml(),
+            "<p size = 3 >  </p>")
 
     def test_small_factory(self):
         factory = SmallFactory()
-        self.assertEqual(factory.createButton().getHtml(),   "<button size = 1 >  </button>")
-        self.assertEqual(factory.createHeader().getHtml(),   "<h size = 1 >  </h>")
-        self.assertEqual(factory.createImage().getHtml(),    "<image size = 1 >  </image>")
-        self.assertEqual(factory.createText().getHtml(),     "<p size = 1 >  </p>")
+        self.assertEqual(
+            factory.createButton().getHtml(),
+            "<button size = 1 >  </button>")
+        self.assertEqual(
+            factory.createHeader().getHtml(),
+            "<h size = 1 >  </h>")
+        self.assertEqual(
+            factory.createImage().getHtml(),
+            "<image size = 1 >  </image>")
+        self.assertEqual(
+            factory.createText().getHtml(),
+            "<p size = 1 >  </p>")
 
     def test_factory_is_singleton_instanse(self):
         factory = LargeFactory()
@@ -48,6 +73,7 @@ class FactoryTest(unittest.TestCase):
         factory = LargeFactory()
         SmallFactory().createButton()
         self.assertNotEqual(factory.instanse, SmallFactory().instanse)
+
 
 if __name__ == '__main__':
     unittest.main()
