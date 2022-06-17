fb9afeeb5c845451b1653fa851dcc1562df012f5
mpilkou <mpilkou@gmail.com>
2022-06-17 10:11:11 +0300

    add changes to adapter

--- 2_Adapter/adapter_object.py
+++ 2_Adapter/adapter_object.py
@@ -1,21 +1,23 @@
+from abc import ABC, abstractclassmethod
 
 
-class Target():
-    def request__devide(self, number_1, number_2):
-        return number_1 / number_2
+class Target(ABC):
+    @abstractclassmethod
+    def request(self, number_1 : object, number_2 : int ) -> float:
+        pass
 
 
 class Adaptee():
-    def specific_request_round(self, number_1, number_2):
-        return round(number_1, number_2)
+    def specific_request(self, number : object) -> float:
+        return number / 10
 
 
 class Adapter(Target):
     _adaptee = None
 
-    def __init__(self, adaptee):
+    def __init__(self, adaptee : Adaptee):
         self._adaptee = adaptee
 
-    def request__devide(self, number_1, number_2):
-        return self._adaptee.specific_request_round(
-            super().request__devide(number_1, number_2), 2)
+    def request(self, number_1 : object, number_2 : int ) -> float:
+        number_1 = self._adaptee.specific_request(number = number_1) * 10
+        return round(number_1, number_2)
